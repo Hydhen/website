199d0f9cd31eced4b8426f03917b38f6648ae0e7
Loïc Juillet <juillet.loic@gmail.com>
2017-02-17 14:38:50 +0100

    add dependencies for angular-material

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 Flask app
 '''
 
-from flask import Flask
+from flask import (Flask, render_template)
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def index_route():
     '''
     Index
     '''
-    return "Hello World!"
+    return render_template("index.html", name="index")
 
 if __name__ == "__main__":
     app.run()

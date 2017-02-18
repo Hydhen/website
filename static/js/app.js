80cc9fa27d01970a28305981ea7392119894ff82
Loïc Juillet <juillet.loic@gmail.com>
2017-02-18 10:31:24 +0100

    add sidenav bar

--- static/js/app.js
+++ static/js/app.js
@@ -4,3 +4,14 @@ app.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('{[{');
     $interpolateProvider.endSymbol('}]}');
 });
+
+app.controller('indexCtrl', function ($scope, $timeout, $mdSidenav) {
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+
+    function buildToggler(componentId) {
+        return function() {
+            $mdSidenav(componentId).toggle();
+        };
+    }
+});
